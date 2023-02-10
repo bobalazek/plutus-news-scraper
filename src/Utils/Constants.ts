f74daac0532bbff67459d45e763b81552829f0f2
Borut Balazek <bobalazek124@gmail.com>
2023-02-10 16:32:20 +0100

    Rename mongo to mongodb

--- src/Utils/Constants.ts
+++ src/Utils/Constants.ts
@@ -5,7 +5,7 @@ export const IS_DEVELOPMENT = process.env.NODE_ENV !== 'prod';
 export const PUPPETEER_EXECUTABLE_PATH = process.env.PUPPETEER_EXECUTABLE_PATH ?? undefined;
 export const RABBITMQ_URL = process.env.RABBITMQ_URL;
 export const REDIS_URL = process.env.REDIS_URL;
-export const MONGO_URL = process.env.MONGO_URL;
+export const MONGODB_URL = process.env.MONGODB_URL;
 export const LOKI_URL = process.env.LOKI_URL;
 
 // Paths
