5a0ca61b6fe7a0a29fa1d859a056cfed8e922b9f
Borut Balazek <bobalazek124@gmail.com>
2023-02-16 16:12:01 +0100

    More zod bits

--- src/Schemas/NewsArticleSchema.ts
+++ src/Schemas/NewsArticleSchema.ts
@@ -1,18 +1,44 @@
 import { z } from 'zod';
 
 import { NewsArticleMultimediaTypeEnum } from '../Types/NewsArticleMultimediaTypeEnum';
+import { NewsArticleTypeEnum } from '../Types/NewsArticleTypeEnum';
+import { ProcessingStatusEnum } from '../Types/ProcessingStatusEnum';
+import { NewsBasicArticleSchema } from './NewsBasicArticleSchema';
 
 export const NewsArticleAuthorSchema = z.object({
   name: z.string(),
   url: z.string().optional(),
 });
+export type NewsArticleAuthorType = z.infer<typeof NewsArticleAuthorSchema>;
 
 export const NewsArticleCategorySchema = z.object({
   name: z.string(),
   url: z.string().optional(),
 });
+export type NewsArticleCategoryType = z.infer<typeof NewsArticleCategorySchema>;
 
-export const NewsArticleSchema = z.object({
+/**
+ * We will need to use a Named Entity Recognition library like:
+ * https://spacy.io
+ * https://huggingface.co/flair/ner-english-ontonotes-large
+ * https://huggingface.co/dslim/bert-base-NER
+ *
+ * Or we could use a GPT for that (https://nlpcloud.com/effectively-using-gpt-j-gpt-neo-gpt-3-alternatives-few-shot-learning.html) like:
+ * GPT-3
+ * GPT-J
+ * GPT-NeoX
+ **/
+export const NewsArticleEntitySchema = z.object({
+  type: z.string(),
+  value: z.string(), // APPL@NASDAQ, Apple Inc., USD, BTC
+  sentiment: z.number().min(-1).max(1).optional(), // What is the sentiment towards this entity in this article?
+});
+export type NewsArticleEntityType = z.infer<typeof NewsArticleEntitySchema>;
+
+/**
+ * This is the schema for our news scraper.
+ */
+export const NewsArticleSchema = NewsBasicArticleSchema.extend({
   title: z.string(),
   multimediaType: z.nativeEnum(NewsArticleMultimediaTypeEnum),
   content: z.string(),
@@ -24,3 +50,21 @@ export const NewsArticleSchema = z.object({
   publishedAt: z.date(),
   modifiedAt: z.date(),
 });
+export type NewsArticleType = z.infer<typeof NewsArticleSchema>;
+
+/**
+ * This schema would mostly be used when we are persisting that data somewhere like
+ * into a file or into the database.
+ */
+export const NewsArticleExtendedSchema = NewsArticleSchema.extend({
+  type: z.nativeEnum(NewsArticleTypeEnum),
+  newsSiteKey: z.string(),
+  entities: z.array(NewsArticleCategorySchema).optional(),
+  tags: z.array(z.string()).optional(),
+  processingStatus: z.nativeEnum(ProcessingStatusEnum).optional(),
+  processingFailedMessage: z.string().optional(),
+  processingStartedAt: z.date().optional(),
+  processingFailedAt: z.date().optional(),
+  processedAt: z.date().optional(),
+});
+export type NewsArticleExtendedType = z.infer<typeof NewsArticleExtendedSchema>;
---
+++ src/Schemas/NewsBasicArticleSchema.ts
@@ -0,0 +1,17 @@
+import { z } from 'zod';
+
+/**
+ * Use this to return the very basic data from the news list pages.
+ * The only required propery is the url, but you can also add the title or content if you want.
+ */
+export const NewsBasicArticleSchema = z.object({
+  url: z.string(),
+  title: z.string().optional(),
+  content: z.string().optional(),
+});
+export type NewsBasicArticleType = z.infer<typeof NewsBasicArticleSchema>;
+
+export const NewsBasicArticleExtendedSchema = NewsBasicArticleSchema.extend({
+  newsSiteKey: z.string(),
+});
+export type NewsBasicArticleExtendedType = z.infer<typeof NewsBasicArticleExtendedSchema>;
