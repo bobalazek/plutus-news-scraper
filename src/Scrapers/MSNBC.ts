23450ff7e1b5bf0a7f28d03d24ecbb8a11ab3b9c
Ana <anakociper124@gmail.com>
2023-02-17 19:31:35 +0100

    Adding scrapers

---
+++ src/Scrapers/MSNBC.ts
@@ -0,0 +1,130 @@
+import { convert } from 'html-to-text';
+
+import { NewsArticleDataNotFoundError } from '../Errors/NewsArticleDataNotFoundError';
+import { NewsArticleType } from '../Schemas/NewsArticleSchema';
+import { NewsBasicArticleType } from '../Schemas/NewsBasicArticleSchema';
+import { logger } from '../Services/Logger';
+import { NewsArticleMultimediaTypeEnum } from '../Types/NewsArticleMultimediaTypeEnum';
+import { NewsScraperInterface } from '../Types/NewsScraperInterface';
+import { AbstractNewsScraper } from './AbstractNewsScraper';
+
+export default class MSNBCNewsScraper extends AbstractNewsScraper implements NewsScraperInterface {
+  key: string = 'msnbc';
+  domain: string = 'www.msnbc.com';
+  recentArticleListUrls: string[] = ['https://www.msnbc.com/', 'https://www.msnbc.com/opinion/columnists'];
+
+  async scrapeRecentArticles(urls?: string[]): Promise<NewsBasicArticleType[]> {
+    const basicArticles: NewsBasicArticleType[] = [];
+    const recentArticleListUrls = Array.isArray(urls) ? urls : this.recentArticleListUrls;
+
+    const page = await this.getPuppeteerPage();
+
+    logger.info(`Starting to scrape the recent articles on MSNBC ...`);
+
+    for (const recentArticleListUrl of recentArticleListUrls) {
+      logger.info(`Going to URL ${recentArticleListUrl} ...`);
+
+      await page.goto(recentArticleListUrl, {
+        waitUntil: 'domcontentloaded',
+      });
+
+      const articleUrls = this.getUniqueArray(
+        await page.evaluate(() => {
+          // Get all the possible (anchor) elements that have the links to articles
+          const querySelector = [
+            '.layout-container .smorgasbord__column1 h2 a',
+            '.layout-container .smorgasbord__column2 h2 a',
+            '.layout-container .feeds .wide-tease-item__info-wrapper a',
+          ].join(', ');
+
+          // Fetch those with the .querySelectoAll() and convert it to an array
+          const $elements = Array.from(document.querySelectorAll(querySelector));
+
+          // Loop/map through those elements and get the href artibute
+          return $elements.map(($el) => {
+            return $el.getAttribute('href') ?? ''; // Needs to have a '' (empty string) as a fallback, because otherwise it could be null, which we don't want
+          });
+        })
+      ).filter((href) => {
+        return href !== ''; // Now we want to filter out any links that are '', just in case
+      });
+
+      logger.info(`Found ${articleUrls.length} articles on this page`);
+
+      for (const articleUrl of articleUrls) {
+        const url = this._preProcessUrl(articleUrl);
+
+        logger.debug(`Article URL: ${url}`);
+
+        basicArticles.push({
+          // We are actually pushing a basic article object, instead of just URL,
+          // if in the future we for example maybe want to provide some more metadata
+          // on the list (recent  and archived articles) scrape
+          url: url,
+        });
+      }
+    }
+
+    await this.closePuppeteerBrowser();
+
+    return Promise.resolve(this.getUniqueArray(basicArticles));
+  }
+
+  async scrapeArticle(basicArticle: NewsBasicArticleType): Promise<NewsArticleType | null> {
+    const page = await this.getPuppeteerPage();
+
+    const url = this._preProcessUrl(basicArticle.url);
+
+    logger.info(`Going to URL ${url} ...`);
+
+    await page.goto(url, {
+      waitUntil: 'domcontentloaded',
+    });
+
+    const categoryUrlSplit = url;
+    const categoryUrl = categoryUrlSplit.substring(0, url.lastIndexOf('/'));
+
+    const linkedDataText = await page.evaluate(() => {
+      return document.querySelector('head script[type="application/ld+json"]')?.innerHTML ?? '';
+    });
+    if (!linkedDataText) {
+      throw new NewsArticleDataNotFoundError(`Linked data not found for URL ${url}`);
+    }
+
+    const linkedData = JSON.parse(linkedDataText);
+
+    // Content
+    const content = await page.evaluate(() => {
+      return Array.from(document.querySelectorAll('.article-body .article-body__content p'))
+        .map((element) => {
+          return element.innerHTML;
+        })
+        .join('');
+    });
+
+    await this.closePuppeteerBrowser();
+
+    const article: NewsArticleType = {
+      url: url,
+      title: linkedData.headline,
+      multimediaType: NewsArticleMultimediaTypeEnum.TEXT,
+      content: convert(content, {
+        wordwrap: false,
+      }),
+      newsSiteArticleId: linkedData.articleId,
+      publishedAt: new Date(linkedData.datePublished),
+      modifiedAt: new Date(linkedData.dateModified),
+      authors: linkedData.author,
+      categories: [{ name: linkedData.articleSection, url: categoryUrl }],
+      imageUrl: linkedData.image.url,
+    };
+
+    return Promise.resolve(article);
+  }
+
+  private _preProcessUrl(url: string): string {
+    const urlObject = new URL(url);
+
+    return url.replace(urlObject.search, '').replace(urlObject.hash, '');
+  }
+}
---
+++ src/Scrapers/NPR.ts
@@ -0,0 +1,144 @@
+import { convert } from 'html-to-text';
+
+import { NewsArticleDataNotFoundError } from '../Errors/NewsArticleDataNotFoundError';
+import { NewsArticleType } from '../Schemas/NewsArticleSchema';
+import { NewsBasicArticleType } from '../Schemas/NewsBasicArticleSchema';
+import { logger } from '../Services/Logger';
+import { NewsArticleMultimediaTypeEnum } from '../Types/NewsArticleMultimediaTypeEnum';
+import { NewsScraperInterface } from '../Types/NewsScraperInterface';
+import { AbstractNewsScraper } from './AbstractNewsScraper';
+
+export default class NPRNewsScraper extends AbstractNewsScraper implements NewsScraperInterface {
+  key: string = 'npr';
+  domain: string = 'www.npr.org';
+  recentArticleListUrls: string[] = [
+    'https://www.npr.org/',
+    'https://www.npr.org/sections/national/',
+    'https://www.npr.org/sections/world/',
+    'https://www.npr.org/sections/politics/',
+    'https://www.npr.org/sections/business/',
+    'https://www.npr.org/sections/health/',
+    'https://www.npr.org/sections/science/',
+    'https://www.npr.org/sections/climate/',
+  ];
+
+  async scrapeRecentArticles(urls?: string[]): Promise<NewsBasicArticleType[]> {
+    const basicArticles: NewsBasicArticleType[] = [];
+    const recentArticleListUrls = Array.isArray(urls) ? urls : this.recentArticleListUrls;
+
+    const page = await this.getPuppeteerPage();
+
+    logger.info(`Starting to scrape the recent articles on NPR ...`);
+
+    for (const recentArticleListUrl of recentArticleListUrls) {
+      logger.info(`Going to URL ${recentArticleListUrl} ...`);
+
+      await page.goto(recentArticleListUrl, {
+        waitUntil: 'domcontentloaded',
+      });
+
+      const articleUrls = this.getUniqueArray(
+        await page.evaluate(() => {
+          // Get all the possible (anchor) elements that have the links to articles
+          const querySelector = ['#main-section .story-text a', '#main-section .item-info a'].join(', ');
+
+          // Fetch those with the .querySelectoAll() and convert it to an array
+          const $elements = Array.from(document.querySelectorAll(querySelector));
+
+          // Loop/map through those elements and get the href artibute
+          return $elements.map(($el) => {
+            return $el.getAttribute('href') ?? ''; // Needs to have a '' (empty string) as a fallback, because otherwise it could be null, which we don't want
+          });
+        })
+      ).filter((href) => {
+        return href !== ''; // Now we want to filter out any links that are '', just in case
+      });
+
+      logger.info(`Found ${articleUrls.length} articles on this page`);
+
+      for (const articleUrl of articleUrls) {
+        const url = this._preProcessUrl(articleUrl);
+
+        logger.debug(`Article URL: ${url}`);
+
+        basicArticles.push({
+          // We are actually pushing a basic article object, instead of just URL,
+          // if in the future we for example maybe want to provide some more metadata
+          // on the list (recent  and archived articles) scrape
+          url: url,
+        });
+      }
+    }
+
+    await this.closePuppeteerBrowser();
+
+    return Promise.resolve(this.getUniqueArray(basicArticles));
+  }
+
+  async scrapeArticle(basicArticle: NewsBasicArticleType): Promise<NewsArticleType | null> {
+    const page = await this.getPuppeteerPage();
+
+    const url = this._preProcessUrl(basicArticle.url);
+
+    logger.info(`Going to URL ${url} ...`);
+
+    await page.goto(url, {
+      waitUntil: 'domcontentloaded',
+    });
+
+    const newsSiteArticleId = await page.evaluate(() => {
+      return document.querySelector('.story .storytitle input:first-child')?.getAttribute('id') ?? '';
+    });
+
+    /*  const categoryName = await page.evaluate(() => {
+      return document.querySelector('.story .slug a')?.innerHTML ?? '';
+    });
+
+    const categoryUrl = await page.evaluate(() => {
+      return document.querySelector('.story .slug')?.getAttribute('href') ?? '';
+    }); */
+
+    const linkedDataText = await page.evaluate(() => {
+      return document.querySelector('head script[type="application/ld+json"]')?.innerHTML ?? '';
+    });
+    if (!linkedDataText) {
+      throw new NewsArticleDataNotFoundError(`Linked data not found for URL ${url}`);
+    }
+
+    const linkedData = JSON.parse(linkedDataText);
+    console.log(linkedData);
+    // Content
+    const content = await page.evaluate(() => {
+      return Array.from(document.querySelectorAll('.storytext p'))
+        .map((element) => {
+          return element.innerHTML;
+        })
+        .join('');
+    });
+
+    await this.closePuppeteerBrowser();
+
+    const article: NewsArticleType = {
+      url: url,
+      title: linkedData.headline,
+      multimediaType: NewsArticleMultimediaTypeEnum.TEXT,
+      content: convert(content, {
+        wordwrap: false,
+      }),
+      newsSiteArticleId: newsSiteArticleId,
+      publishedAt: new Date(linkedData.datePublished),
+      modifiedAt: new Date(linkedData.dateModified),
+      // authors: linkedData.author.name[0],
+      // categories: [{ name: categoryName, url: categoryUrl }],
+      // imageUrl: linkedData.image,
+    };
+
+    return Promise.resolve(article);
+  }
+
+  private _preProcessUrl(url: string): string {
+    const urlObject = new URL(url);
+
+    return url.replace(urlObject.search, '').replace(urlObject.hash, '');
+  }
+}
