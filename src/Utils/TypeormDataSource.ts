ffed3adc0894fa46add9857f0ecc86973afb850d
Borut <bobalazek124@gmail.com>
2023-04-14 16:04:14 +0200

    Closer with the whole typeorm stuff

--- src/Utils/TypeormDataSource.ts
+++ src/Utils/TypeormDataSource.ts
@@ -1,13 +1,15 @@
+import { join } from 'path';
 import { DataSource } from 'typeorm';
 
 import { IS_DEVELOPMENT, POSTGRESQL_URL } from './Environment';
+import { ROOT_DIRECTORY } from './Paths';
 
 const dataSource = new DataSource({
   type: 'postgres',
   url: POSTGRESQL_URL,
   synchronize: IS_DEVELOPMENT,
-  entities: ['src/Entities/*.ts'],
-  migrations: ['src/Migrations/*.ts'],
+  entities: [join(ROOT_DIRECTORY, 'Entities', '*.{ts,js}')],
+  migrations: [join(ROOT_DIRECTORY, 'Migrations', '*.{ts,js}')],
 });
 
 export default dataSource;
